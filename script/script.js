b12bbe2f5130aff9d3aed721cf5c8a893c6e4970
Andquha <andruhaarh98@gmail.com>
2022-07-12 16:20:53 +0300

    First_Advantage_page

--- script/script.js
+++ script/script.js
@@ -1,5 +1,65 @@
-$(document).ready(function(){
-});
+$(document).ready(function () {
+    $('.main_advantage_box_content_slider').slick({
+      adaptiveHeight: true,
+      slidesToShow: 4,
+      slidestoScroll: 1,
+      speed: 3000,
+      easing: 'ease',
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      swipe: true,
+      nextArrow: '.main_advantage_box_content_slider_arrows_right',
+      prevArrow: '.main_advantage_box_content_slider_arrows_left',
+      responsive:[
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+      ]
+    });
+    // $('.ourwork_slides').slick({
+    //   adaptiveHeight: true,
+    //   slidesToShow: 1,
+    //   slidestoScroll: 1,
+    //   speed: 1000,
+    //   easing: 'ease',
+    //   infinite: true,
+    //   autoplay: true,
+    //   autoplaySpeed: 10000,
+    //   variableWidth: true,
+    //   swipe: true,
+    //   centerMode: true,
+    //   focusOnSelect: true,
+    //   nextArrow: '.ourwork.arrow_right',
+    //   prevArrow: '.ourwork.arrow_left',
+    //   asNavFor: '.ourwork_slides_focus',
+    // });
+    // $('.ourwork_slides_focus').slick({
+    //   adaptiveHeight: true,
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1,
+    //   arrows: false,
+    //   fade: true,
+    //   asNavFor: '.ourwork_slides',
+    // });
+    // $('.reviews_slides').slick({
+    //   adaptiveHeight: true,
+    //   slidesToShow: 1,
+    //   slidestoScroll: 1,
+    //   speed: 1000,
+    //   easing: 'ease',
+    //   infinite: true,
+    //   autoplay: true,
+    //   autoplaySpeed: 5000,
+    //   swipe: true,
+    //   nextArrow: '.reviews.arrow_right',
+    //   prevArrow: '.reviews.arrow_left',
+  
+    // });
+  });
 
 
 
